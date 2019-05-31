6cca792279697828a576d9beaa269aa283b6c640
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2019-05-31 15:11:14 +0300

    puhelinluettelo backend step8

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 
 app.use(bodyParser.json())
+app.use(morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-', 
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(" ")
+}))
 
 const PORT = 3001
 
