8b54a558e2827fd395514f87a5d088313745cd74
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2019-08-23 18:43:54 +0300

    -> 3.14

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(cors())
@@ -18,30 +20,7 @@ app.use(morgan((tokens, req, res) => {
     ].join(" ")
 }))
 
-const PORT = process.env.PORT || 3001
-
-let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1
-    },
-    {
-      name: "Martti Tienari",
-      number: "040-123456",
-      id: 2
-    },
-    {
-      name: "Arto Järvinen",
-      number: "040-123456",
-      id: 3
-    },
-    {
-      name: "Lea Kutvonen",
-      number: "040-123456",
-      id: 4
-    }
-]
+const PORT = process.env.PORT
 
 app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id)
@@ -51,40 +30,36 @@ app.delete("/api/persons/:id", (req, res) => {
 
 app.post("/api/persons", (req, res) => {
     p = req.body
-    const id = Math.floor(Math.random() * 10000000)
-    if (p.name && p.number) {
-        if (persons.find(pe => pe.name === p.name)) {
-            return res.status(400).json({error: "Name must be unique!"})
-        }
-        p = {
-            name: p.name,
-            number: p.number,
-            id: id
-        }
-        persons = persons.concat(p)
-        return res.json(p)
+    if (p.name === undefined || p.number === undefined) {
+        res.status(400).json({error: "Name and number fields are required!"});
     }
-    res.status(400).json({error: "Name and number fields are required!"})
-})
+
+    const person = new Person({
+        name: p.name,
+        number: p.number
+    });
+    person.save().then(nPerson => {
+        res.json(nPerson.toJSON());
+    });
+});
 
 app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id)
-    p = persons.find(p => p.id === id)
-    if (p) {
-        res.json(p)
-    }
-    else {
-        res.status(404).json({error: "No such person!"})
-    }
-})
+    Person.findById(req.params.id).then(person => {
+        response.json(person.toJSON());
+    });
+});
 
 app.get("/info", (req, res) => {
-    res.send(`<p>Puhelinluettelossa on ${persons.length} henkilön tiedot</p><p>${Date().toString()}</p>`)
-})
+    people.find({}).then(result => {
+        res.send(`<p>Puhelinluettelossa on ${result.length} henkilön tiedot</p><p>${Date().toString()}</p>`);
+    })
+});
 
 app.get("/api/persons", (_, res) => {
-    res.json(persons)
-})
+    Person.find({}).then(result => {
+        res.json(result.map(p => p.toJSON()));
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Running on ${PORT}`)
