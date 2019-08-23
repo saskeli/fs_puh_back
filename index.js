6eddc1b81f0d518b2093d9fd53e52ef86c500c62
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2019-08-23 19:48:36 +0300

    -> 3.18

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const app = express()
 const Person = require('./models/person')
+
+const app = express()
 
 app.use(express.static('build'))
 app.use(cors())
@@ -23,9 +24,9 @@ app.use(morgan((tokens, req, res) => {
 const PORT = process.env.PORT
 
 app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id).then(result => {
+        res.status(204).end()
+    }).catch(error => next(error));
 })
 
 app.post("/api/persons", (req, res) => {
@@ -40,27 +41,62 @@ app.post("/api/persons", (req, res) => {
     });
     person.save().then(nPerson => {
         res.json(nPerson.toJSON());
-    });
+    }).catch(error => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
     Person.findById(req.params.id).then(person => {
-        res.json(person.toJSON());
-    });
+        if (person) {
+            res.json(person.toJSON());
+        } else {
+            res.status(404).end();
+        }
+    }).catch(error => next(error));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
+    
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+  
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updated => {
+            res.json(updated.toJSON());
+        }).catch(error => next(error));
 });
 
 app.get("/info", (req, res) => {
     Person.find({}).then(result => {
         res.send(`<p>Puhelinluettelossa on ${result.length} henkilön tiedot</p><p>${Date().toString()}</p>`);
-    })
+    }).catch(error => next(error));
 });
 
 app.get("/api/persons", (_, res) => {
     Person.find({}).then(result => {
         res.json(result.map(p => p.toJSON()));
-    });
+    }).catch(error => next(error));
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on ${PORT}`)
-})
+    console.log(`Running on ${PORT}`);
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+}
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+  
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' });
+    } 
+    next(error);
+}
+
+app.use(errorHandler);
